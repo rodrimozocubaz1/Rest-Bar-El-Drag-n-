913425f07a9c460bf14d11318bbbb7eb049cddb1
Patrick Caire Torres <caire_10@outlook.com>
2019-11-12 14:24:48 -0500

    AvanceFinal

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -14,7 +14,7 @@ namespace Rest_Bar_El_Drag_n_.Controllers
         }
         public IActionResult Index()
         {
-            var lista = _context.Menus.ToList();
+            var lista = _context.Menus.Take(15).ToList();
             return View(lista);
         }
         [Authorize(Roles="Administrador")]
@@ -25,9 +25,28 @@ namespace Rest_Bar_El_Drag_n_.Controllers
         }
         [Authorize(Roles="Administrador")]
         [HttpPost]
+        /*public Menu Menu {get;set;}   int id*/
         public IActionResult Registro(Menu x)
         {
+            
             if (ModelState.IsValid) {
+                /*var _Menu = _context.Menus.Where(x=>x.Id == Menu.Id).SingleOrDefault();
+                if(_Cliente == null)
+                {
+                    _context.Menus.Add(Menu);
+                }
+                else
+                {
+                    _Menu.Nombre = Menu.Nombre;
+                    _Menu.Foto = Menu.Foto;
+                    _Menu.Descripcion = Menu.Descripcion;
+                    _Menu.Precio = Menu.Precio;
+                    _Menu.Puntos = Menu.Puntos;
+                    _Menu.CategoriaId = Menu.CategoriaId;
+                }
+                _context.SaveChanges();
+                return RedirectToAction("ver_menus");*/
+
                 _context.Add(x);
                 _context.SaveChanges();
                 return RedirectToAction("Index");
@@ -35,10 +54,49 @@ namespace Rest_Bar_El_Drag_n_.Controllers
             ViewBag.Categorias = _context.Categorias.ToList();
             return View(x);
         }
+        /*public IActionResult ver_menus(){
+            ViewBag.Menus = _context.Menus.ToList();
+            return View();
+        }
+        
+        public IActionResult Modificar(int id)
+        {
+            var Menu = _context.Menus.Find(id);
+            if(Menu == null)
+            {
+                return RedirecToAction("ver_menus");
+            }            
+            return RedirecToAction("Registro");
+        }
+
+        
+        public IActionResult Eliminar(int id)
+        {
+            var Menu = _context.Menus.Find(id);
+            if(Menu == null)
+            {
+                return StatusCode(404);
+            }
+            _context.Menus.Remove(Menu);
+            _context.SaveChanges();
+            return Redirect("ver_menus");
+        }
+
+        */
+
+
         public IActionResult Menuxcateg(int id){
             var lista= _context.Menus.Where(x=>x.CategoriaId==id).ToList();
             return View(lista);
         }
+        /*
+        public IActionResult Comprar(int id){
+            var lista= _context.Menus.Where(x=>x.CategoriaId==id).ToList();
+            return View(lista);
+        }
+
+
+        */
         
     }
 }
